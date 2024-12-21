e8e180e0111d1701015e969bd9b6aa38336e4e6b
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-12-22 02:18:00 +0530

    Done For Now

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './components/About';
-import Home from './components/Home';
+
 import "./index.css";
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutme" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
+      <About />
       <Footer />
     </div>
   );
